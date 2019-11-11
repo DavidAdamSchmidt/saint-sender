206e00e07827230b506c24c51ae2a37dff1f07f4
David Schmidt <david.adam.schmidt@gmail.com>
2019-11-11 18:29:48 +0100

    Clear login fields when clicking on Cancel button
    TODO: Change the password field from TextBox to PasswordBox

--- SaintSender.DesktopUI/ViewModels/LoginWindowModel.cs
+++ SaintSender.DesktopUI/ViewModels/LoginWindowModel.cs
@@ -1,6 +1,37 @@
-﻿namespace SaintSender.DesktopUI.ViewModels
+﻿using SaintSender.Core.Entities;
+
+namespace SaintSender.DesktopUI.ViewModels
 {
-    public class LoginWindowModel
+    public class LoginWindowModel : ViewModelBase
     {
+        private string _email;
+        private string _password;
+
+        public LoginWindowModel()
+        {
+            SetCommands();
+        }
+
+        public string Email { get => _email; set => SetProperty(ref _email, value); }
+
+        public string Password { get => _password; set => SetProperty(ref _password, value); }
+
+        public DelegateCommand<string> CancelButtonClickCommand { get; private set; }
+
+        private void SetCommands()
+        {
+            SetCancelButtonCommand();
+        }
+
+        private void SetCancelButtonCommand()
+        {
+            CancelButtonClickCommand = new DelegateCommand<string>(CancelLogin);
+        }
+
+        private void CancelLogin(string s)
+        {
+            Email = string.Empty;
+            Password = string.Empty;
+        }
     }
 }
