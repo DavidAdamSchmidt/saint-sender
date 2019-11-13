1576911d2f374b8e7d127fb4dc1bb17e68489e0d
David Schmidt <david.adam.schmidt@gmail.com>
2019-11-13 23:18:33 +0100

    Disable LoginWindow buttons when Spinner is active

--- SaintSender.DesktopUI/ViewModels/LoginWindowModel.cs
+++ SaintSender.DesktopUI/ViewModels/LoginWindowModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using SaintSender.Core.Entities;
 using SaintSender.Core.Services;
@@ -31,7 +29,12 @@ namespace SaintSender.DesktopUI.ViewModels
         public bool IsSending
         {
             get => _sending;
-            set => SetProperty(ref _sending, value);
+            set
+            {
+                SetProperty(ref _sending, value);
+                CancelButtonClickCommand.RaiseCanExecuteChanged();
+                SignInButtonClickCommand.RaiseCanExecuteChanged();
+            }
         }
 
         public DelegateCommand<PasswordBox> CancelButtonClickCommand { get; private set; }
@@ -43,7 +46,7 @@ namespace SaintSender.DesktopUI.ViewModels
         private void SetCommands()
         {
             CancelButtonClickCommand = new DelegateCommand<PasswordBox>(CancelLogin_Execute, CancelLogin_CanExecute);
-            SignInButtonClickCommand = new DelegateCommand<PasswordBox>(AuthenticateLogin_Execute);
+            SignInButtonClickCommand = new DelegateCommand<PasswordBox>(AuthenticateLogin_Execute, AuthenticateLogin_CanExecute);
             PasswordChangedCommand = new DelegateCommand<string>(UpdateCancelLoginAvailability_Execute);
         }
 
@@ -55,7 +58,9 @@ namespace SaintSender.DesktopUI.ViewModels
 
         private bool CancelLogin_CanExecute(PasswordBox passwordBox)
         {
-            return !string.IsNullOrWhiteSpace(Email) || !string.IsNullOrWhiteSpace(passwordBox?.Password);
+            return (!string.IsNullOrWhiteSpace(Email) ||
+                    !string.IsNullOrWhiteSpace(passwordBox?.Password)) &&
+                   !_sending; ;
         }
 
         private async void AuthenticateLogin_Execute(PasswordBox passwordBox)
@@ -80,6 +85,11 @@ namespace SaintSender.DesktopUI.ViewModels
                 MessageBox.Show("Wrong email or password provided.", "Credentials Alert", MessageBoxButton.OK,
                     MessageBoxImage.Exclamation);
             }
+        }
+
+        private bool AuthenticateLogin_CanExecute(PasswordBox passwordBox)
+        {
+            return !_sending;
         }
 
         private void UpdateCancelLoginAvailability_Execute(string s)
