70c84a2a87df2482631a3e2ffbacf294c966457a
gergicsmilan <gergics.milan95@gmail.com>
2019-11-13 16:01:11 +0100

    created main email details window and window model, converter for email date

---
+++ SaintSender.DesktopUI/Converters/DateTimeConverterForEmailDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SaintSender.DesktopUI.Converters
+{
+    public class DateTimeConverterForEmailDate : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dateTime = (DateTime)value;
+            var formattedDateTime = dateTime.ToString("dddd, dd MMM yyyy h:mm");
+            return formattedDateTime;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dateString = (string)value;
+            var format = "dddd, dd MMM yyyy h:mm";
+            return DateTime.ParseExact(dateString, format, culture);
+        }
+    }
+}
