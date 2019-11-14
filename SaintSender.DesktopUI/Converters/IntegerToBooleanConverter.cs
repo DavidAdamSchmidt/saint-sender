9cd09f38d65c3d8c9e55eb78b0841f29c9641fd3
David Schmidt <david.adam.schmidt@gmail.com>
2019-11-14 18:56:19 +0100

    Disable ComboBox dropdown when no saved data found

---
+++ SaintSender.DesktopUI/Converters/IntegerToBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SaintSender.DesktopUI.Converters
+{
+    public class IntegerToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int i && i > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b && b ? 1 : 0;
+        }
+    }
+}
