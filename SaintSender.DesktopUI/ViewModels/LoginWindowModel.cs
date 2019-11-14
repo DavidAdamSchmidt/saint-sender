0f467dd514486e71ca50e531de20538e6912eeb9
John <John>
2019-11-14 15:52:06 +0100

    uncommited changes

--- SaintSender.DesktopUI/ViewModels/LoginWindowModel.cs
+++ SaintSender.DesktopUI/ViewModels/LoginWindowModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using SaintSender.Core.Entities;
 using SaintSender.Core.Services;
@@ -39,37 +37,17 @@ namespace SaintSender.DesktopUI.ViewModels
             }
         }
 
-        private Dictionary<string, string> _userData = EncryptionService.RetreiveAllData();
-
-        public Dictionary<string, string>.KeyCollection Users
-        {
-            get => _userData.Keys;
-        }
-
-        public string SelectedUser { get; set; }
-
         public DelegateCommand<PasswordBox> CancelButtonClickCommand { get; private set; }
 
         public DelegateCommand<PasswordBox> SignInButtonClickCommand { get; private set; }
 
         public DelegateCommand<string> PasswordChangedCommand { get; private set; }
 
-        public DelegateCommand<PasswordBox> FillLoginDetails { get; private set; }
-
-
-
         private void SetCommands()
         {
             CancelButtonClickCommand = new DelegateCommand<PasswordBox>(CancelLogin_Execute, CancelLogin_CanExecute);
             SignInButtonClickCommand = new DelegateCommand<PasswordBox>(AuthenticateLogin_Execute, AuthenticateLogin_CanExecute);
             PasswordChangedCommand = new DelegateCommand<string>(UpdateCancelLoginAvailability_Execute);
-            FillLoginDetails = new DelegateCommand<PasswordBox>(AutofillLoginDetails_Execute);
-        }
-
-        private void AutofillLoginDetails_Execute(PasswordBox passwordBox)
-        {
-            Email = SelectedUser;
-            passwordBox.Password = _userData[SelectedUser];
         }
 
         private void CancelLogin_Execute(PasswordBox passwordBox)
